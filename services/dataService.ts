96c242ff2f70465b9f828f830d43ed430c5cf4a1
ETN G3 TREINAMENTO <67908706+SohLamentos@users.noreply.github.com>
2026-05-23 23:33:50 -0300

    Update dataService.ts

--- services/dataService.ts
+++ services/dataService.ts
@@ -3171,6 +3171,7 @@ if (incomingGroup === 'FUSO_1') {
 // DEFAULT pode dividir com DEFAULT ou AC.
 // DEFAULT não pode dividir com fuso -1.
 if (existingGroups.includes('FUSO_1')) return false;
+    };
 
 return true;
 
